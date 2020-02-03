f77899d2764a51422bfc1cebd6f9d0707a4a89c8
Dhruv Batheja <dhruv.batheja@booking.com>
2020-02-03 14:55:09 +0100

    Creating a Discrepancy Sink Factory Implementation

---
+++ service/src/main/java/com/booking/validator/service/DiscrepancySinkFactory.java
@@ -0,0 +1,108 @@
+package com.booking.validator.service;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class DiscrepancySinkFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DiscrepancySinkFactory.class);
+
+    private enum Types {
+        KAFKA("kafka"),
+        CONSOLE("console");
+
+        private String value;
+        Types(String value) {
+            this.value = value;
+        }
+        public String getValue() {
+            return value;
+        }
+    }
+
+    public static class DiscrepancySinkMessage {
+        @JsonProperty("task")
+        private String task;
+
+        @JsonProperty("discrepancy")
+        private String discrepancy;
+
+        public DiscrepancySinkMessage(String task, String discrepancy) {
+            this.task = task;
+            this.discrepancy = discrepancy;
+        }
+    }
+
+    public static interface DiscrepancySink {
+        public void send(String message);
+    }
+
+    public static DiscrepancySink getDiscrepancySink(String type, Map<String, String> config) {
+        switch (Types.valueOf(type)) {
+            case CONSOLE:
+                ConsoleDiscrepancySink consoleDiscrepancySink = new ConsoleDiscrepancySink(config);
+                return consoleDiscrepancySink;
+            case KAFKA:
+                KafkaDiscrepancySink kafkaDiscrepancySink = new KafkaDiscrepancySink(config);
+                return kafkaDiscrepancySink;
+            default:
+                return null;
+        }
+    }
+
+    private static class ConsoleDiscrepancySink implements DiscrepancySink {
+        public ConsoleDiscrepancySink(Map<String, String> config) {}
+        @Override
+        public void send(String message) {
+            System.out.println(message);
+        }
+    }
+
+    private static class KafkaDiscrepancySink implements DiscrepancySink {
+        private String topic;
+        private Producer<String, String> producer;
+
+        public static interface Configuration{
+            static final String BROKER_LIST = "broker.list";
+            static final String TOPIC = "topic";
+        }
+
+        public KafkaDiscrepancySink(Map<String, String> config) {
+            String brokerList = config.get(Configuration.BROKER_LIST);
+            String topic = config.get(Configuration.TOPIC);
+            if (brokerList.isEmpty() || topic.isEmpty()) {
+                throw new RuntimeException("Kafka Discrepancy Sink needs broker-list and topic");
+            }
+            this.topic = topic;
+            this.producer = setupKafkaProducer(brokerList);
+        }
+
+        private Producer<String, String> setupKafkaProducer(String brokerList) {
+            Producer<String,String> producer = null;
+            Properties properties = new Properties();
+            properties.put("bootstrap.servers", brokerList);
+            try {
+                producer = new KafkaProducer(properties, new StringSerializer(), new StringSerializer());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return producer;
+        }
+
+        @Override
+        public void send(String message) {
+            try{
+                producer.send(new ProducerRecord<>(topic, message));
+            } catch (Exception e) {
+                LOGGER.error(e.getStackTrace().toString());
+            }
+        }
+    }
+}
